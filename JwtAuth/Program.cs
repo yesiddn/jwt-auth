28d28218aff8766261db9ea61c963ca8762bfbee
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-07 17:27:38 -0500

    Add DbContext service and necessary using directives
    Added using directives for JwtAuth.Data and Microsoft.EntityFrameworkCore.
    Configured DbContext service to use SQL Server with a connection string
    from the application's configuration. This setup allows the application
    to interact with a SQL Server database using Entity Framework Core.

--- JwtAuth/Program.cs
+++ JwtAuth/Program.cs
@@ -1,3 +1,5 @@
+using JwtAuth.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<UserDbContext>(options =>
+{
+  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
