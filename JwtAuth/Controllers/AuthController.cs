0bd3d5d5b9dca70197a50c62d1494f17f2686ba7
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-07 16:15:22 -0500

    Add AuthController for user registration and login
    Introduced a new AuthController class to manage user authentication.
    Implemented Register method to hash passwords and store user details.
    Implemented Login method to verify credentials and return a token.
    Imported necessary namespaces for authentication and API handling.

---
+++ JwtAuth/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using JwtAuth.Entities;
+using JwtAuth.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuth.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AuthController : ControllerBase
+  {
+    public static User user = new();
+
+    [HttpPost("register")]
+    public ActionResult<User> Register(UserDto request)
+    {
+      var hashedPassword = new PasswordHasher<User>()
+        .HashPassword(user, request.Password);
+
+      user.Username = request.Username;
+      user.PasswordHash = hashedPassword;
+
+      return Ok(user);
+    }
+
+    [HttpPost("login")]
+    public ActionResult<string> Login(UserDto request)
+    {
+      if (user.Username != request.Username)
+      {
+        return BadRequest("Invalid username or password");
+      }
+
+      if (new PasswordHasher<User>()
+        .VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+      {
+        return BadRequest("Invalid username or password");
+      }
+
+      string token = "token";
+      return Ok(token);
+    }
+  }
+}
