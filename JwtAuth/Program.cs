25e09f11ed37eee769d7871c38b274e700abf4fd
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-07 16:15:04 -0500

    Add Scalar.AspNetCore and configure OpenApi service
    Added a new using directive for Scalar.AspNetCore. Modified Program.cs to include the AddOpenApi service in the service container. Configured app.MapScalarApiReference() in the HTTP request pipeline for the development environment.

--- JwtAuth/Program.cs
+++ JwtAuth/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,7 +13,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+  app.MapOpenApi();
+  app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
