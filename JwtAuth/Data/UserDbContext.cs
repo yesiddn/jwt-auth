4559d2f9117334efa7736438fb8c660f89bbf8b4
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-07 17:27:05 -0500

    Add UserDbContext for managing User entities
    Introduced a new UserDbContext class in the JwtAuth.Data namespace.
    This class inherits from DbContext and is configured using
    DbContextOptions<UserDbContext>. Added a DbSet<User> property
    named Users to manage User entities, enabling querying and
    saving instances of User in the database.

---
+++ JwtAuth/Data/UserDbContext.cs
@@ -0,0 +1,10 @@
+﻿using JwtAuth.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuth.Data
+{
+  public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
+  {
+    public DbSet<User> Users { get; set; }
+  }
+}
