813d7fdb022ca402a8061132a6622aae9d7cbb74
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-07 22:00:32 -0500

    Add refresh token properties to User class
    Two new properties have been added to the `User` class to support refresh token functionality. The `RefreshToken` property will hold the refresh token as a nullable string, and the `RefreshTokenExpiryTime` property will store the expiration time of the refresh token as a nullable `DateTime`.

--- JwtAuth/Entities/User.cs
+++ JwtAuth/Entities/User.cs
@@ -6,5 +6,7 @@
     public string Username { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
     public string Role { get; set; } = string.Empty;
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
   }
 }
