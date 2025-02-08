e028c35dd7e2dd4c4cadf1acbcc606d19ce0e87c
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-07 22:00:49 -0500

    Add RefreshToken columns to Users table
    Introduce migration `20250208021846_AddRefresToken` to add `RefreshToken` and `RefreshTokenExpiryTime` columns to the `Users` table in the `JwtAuth` project. Update `UserDbContextModelSnapshot.cs` to reflect these changes. Auto-generated `20250208021846_AddRefresToken.Designer.cs` includes the necessary code for the target model. Migration logic for applying and reverting changes is in `20250208021846_AddRefresToken.cs`.

--- JwtAuth/Migrations/20250208021846_AddRefresToken.Designer.cs
+++ JwtAuth/Migrations/20250208021846_AddRefresToken.Designer.cs
@@ -4,6 +4,7 @@ using JwtAuth.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JwtAuth.Migrations
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250208021846_AddRefresToken")]
+    partial class AddRefresToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
