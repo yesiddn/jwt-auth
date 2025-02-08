33172c6953ddbfbd1c428f3a47b645ed5b53c9c5
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-07 21:11:43 -0500

    Add Role property to User class
    A new property `Role` of type `string` has been added to the `User` class. This property is initialized with an empty string by default. This change allows for storing the role or permission level of the user within the application.

--- JwtAuth/Entities/User.cs
+++ JwtAuth/Entities/User.cs
@@ -5,5 +5,6 @@
     public Guid Id { get; set; }
     public string Username { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
   }
 }
