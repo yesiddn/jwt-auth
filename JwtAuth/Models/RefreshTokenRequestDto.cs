77b201821189243ff4039e79a80d4c95dabe5709
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-02-07 22:01:06 -0500

    Add DTOs for handling JWT token requests and responses
    Two new classes, `RefreshTokenRequestDto` and `TokenResponseDto`, have been added to the `JwtAuth.Models` namespace.
    
    - `RefreshTokenRequestDto` includes properties for `UserId` (Guid) and `RefreshToken` (string, required).
    - `TokenResponseDto` includes properties for `AccessToken` and `RefreshToken` (both string, required).
    
    These DTOs facilitate the handling of JWT authentication tokens.

---
+++ JwtAuth/Models/RefreshTokenRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtAuth.Models
+{
+  public class RefreshTokenRequestDto
+  {
+    public Guid UserId { get; set; }
+    public required string RefreshToken { get; set; }
+  }
+}
---
+++ JwtAuth/Models/TokenResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtAuth.Models
+{
+  public class TokenResponseDto
+  {
+    public required string AccessToken { get; set; }
+    public required string RefreshToken { get; set; }
+  }
+}
